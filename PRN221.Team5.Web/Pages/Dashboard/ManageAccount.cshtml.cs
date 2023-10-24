b903a36284c9e0294776a11f1babfc33576510bf
thiennhi <thienphamnhi@gmail.com>
2023-10-24 10:39:45 +0700

    update manage account

--- PRN221.Team5.Web/Pages/Dashboard/ManageAccount.cshtml.cs
+++ PRN221.Team5.Web/Pages/Dashboard/ManageAccount.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -16,9 +17,90 @@ namespace PRN221.Team5.Web.Pages.Dashboard
         {
             _logger = logger;
         }
+        public enum Role
+        {
+            Admin,
+            Staff,
+            ZooTrainer
+        }
+
+        public class Account
+        {
+            public string Username { get; set; }
+            public string Fullname { get; set; }
+            public string Email { get; set; }
+            public string Phone { get; set; }
+            public string Role { get; set; }
+
+            public string Age { get; set; }
+
+            public string Status { get; set; }
+
+            public string CreateDate { get; set; }
+
+            public string UpdateDate { get; set; }
+
+            public string CreateBy { get; set; }
+            public string UpdateBy { get; set; }
+        }
+
+
+
+        [BindProperty]
+        public List<Account> Accounts { get; set; } = new List<Account>();
+        [BindProperty]
+        public Dictionary<Role, string> Roles { get; } = new Dictionary<Role, string>
+        {
+            { Role.Admin, "Admin" },
+            { Role.Staff, "Staff" },
+            { Role.ZooTrainer, "Zoo Trainer" }
+
+        };
+        [BindProperty]
+        public Dictionary<string, string> Statuses { get; } = new Dictionary<string, string>
+        {
+            { "Active", "Active" },
+            { "Inactive", "Inactive" },
+        };
+
 
         public void OnGet()
         {
+            var accounts = new List<Account>{
+                new Account
+                {
+                    Username = "nguyenvana",
+                    Fullname = "Nguyen Van A",
+                    Email = "vana222@gmail.com",
+                    Phone = "0123456789",
+                    Role = Roles[Role.Staff],
+                    Age = "20",
+                    Status = "Active",
+                    CreateDate = "2021-01-01",
+                    UpdateDate = "2021-01-01",
+                    CreateBy = "admin",
+                    UpdateBy = "admin"
+                },
+                new Account {
+                    Username = "nguyenvanb",
+                    Fullname = "Nguyen Van B",
+                    Email = "vanb333@gmail.com",
+                    Phone = "0123456789",
+                    Role = Roles[Role.ZooTrainer],
+                    Age = "40",
+                    Status = "Active",
+                    CreateDate = "2021-01-01",
+                    UpdateDate = "2022-01-01",
+                    CreateBy = "admin",
+                    UpdateBy = "admin"
+                },
+            };
+
+            Accounts = accounts;
+        }
+
+        public async Task<IActionResult> OnPost(){
+            return Page();;
         }
     }
 }
