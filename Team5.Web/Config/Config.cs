d4f24894f6694c027ea8b299d68f0383b75e03cf
khoabui <buidangkhoa05@gmail.com>
2023-11-14 18:34:48 +0700

    (...) update code

--- Team5.Web/Config/Config.cs
+++ Team5.Web/Config/Config.cs
@@ -3,6 +3,11 @@
 //using Team5.Application.Repository;
 //using Team5.Infrastructure.Repository;
 
+using PRN221.Team5.Application.Service.Implement;
+using PRN221.Team5.Application.Service.Interface;
+using Team5.Application.Repository;
+using Team5.Infrastructure.Repository;
+
 namespace Team5.Web.Config
 {
     public static class Config
@@ -14,13 +19,13 @@ namespace Team5.Web.Config
             //    .AddEntityFrameworkStores<IdsDbContext>()
             //    .AddDefaultTokenProviders();
 
-            //services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            //services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
 
-            //services.AddScoped<IAuthService, AuthService>();
-            //services.AddScoped<IAnimalService, AnimalService>();
-            //services.AddScoped<ICageService, CageService>();
-            //services.AddScoped<ITrainerService, TrainerService>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IAnimalService, AnimalService>();
+            services.AddScoped<ICageService, CageService>();
+            services.AddScoped<ITrainerService, TrainerService>();
         }
 
 
