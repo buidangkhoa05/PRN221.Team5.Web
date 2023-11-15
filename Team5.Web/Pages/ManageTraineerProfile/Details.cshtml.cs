e2a9b99bfe6ec80ad231ae1392ddc1249febecf2
Jinergenkai <jinergenkai@gmail.com>
2023-11-15 14:38:25 +0700

    update code

--- Team5.Web/Pages/ManageTraineerProfile/Details.cshtml.cs
+++ Team5.Web/Pages/ManageTraineerProfile/Details.cshtml.cs
@@ -5,37 +5,77 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using PRN221.Team5.Domain.Entity;
+using Team5.Application.Repository;
+using Team5.Domain.Common;
 //using Domain1;
 
 namespace Team5.Web.Pages.ManageTraineerProfile
 {
     public class DetailsModel : PageModel
     {
-        //private readonly Domain1.ZooManagementContext _context;
-
-        //public DetailsModel(Domain1.ZooManagementContext context)
-        //{
-        //    _context = context;
-        //}
-
-      public TraineerProfile TraineerProfile { get; set; } = default!; 
-
-        public async Task<IActionResult> OnGetAsync(Guid? id)
+        private readonly IUnitOfWork _unitOfWork;
+        public DetailsModel(IUnitOfWork unitOfWork)
         {
-            //if (id == null || _context.TraineerProfiles == null)
-            //{
-            //    return NotFound();
-            //}
+            _unitOfWork = unitOfWork;
+        }
 
-            //var traineerprofile = await _context.TraineerProfiles.FirstOrDefaultAsync(m => m.Id == id);
-            //if (traineerprofile == null)
-            //{
-            //    return NotFound();
-            //}
-            //else 
-            //{
-            //    TraineerProfile = traineerprofile;
-            //}
+        public TraineerProfile TraineerProfile { get; set; } = default!;
+        public IList<Animal> Animal { get; set; } = default!;
+        public IList<AnimalTraining> AnimalTraining { get; set; } = default!;
+        public int PageIndex { get; set; } = 1;
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; } = 5;
+        public bool HasNextPage => PageIndex < TotalPages;
+        public bool HasPreviousPage => PageIndex > 1;
+        public string? SearchString { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid id, string? searchString, int? pageIndex)
+        {
+            try
+            {
+                if (id == Guid.Empty)
+                {
+                    return NotFound();
+                }
+
+                var traineerProfile = (await _unitOfWork.TrainerProfile.Get(
+                    new QueryHelper<TraineerProfile>()
+                    {
+                        Filter = t => t.Id == id,
+                        Include = t => t.Include(t => t.Account)
+                    })).ToList();
+                if (traineerProfile == null)
+                {
+                    return NotFound();
+                }
+
+                TraineerProfile = traineerProfile[0];
+                if (pageIndex != null)
+                {
+                    PageIndex = pageIndex.Value;
+                }
+                if (searchString != null)
+                {
+                }
+                else
+                {
+                    AnimalTraining = (await _unitOfWork.AnimalTraining.GetWithPagination(new QueryHelper<AnimalTraining>()
+                    {
+                        Filter = t => t.TrainerId == id,
+                        PagingParams = new PagingParameters(PageIndex, PageSize),
+                        OrderByFields = new List<string>
+                        {
+                            $"UpdatedDate:desc"
+                        }.ToArray(),
+                        Include = t => t.Include(t => t.Animal)
+                    })).ToList();
+                }
+                var count = (await _unitOfWork.AnimalTraining.Get(new QueryHelper<AnimalTraining>() { Filter = t => t.TrainerId == id })).ToList().Count;
+                TotalPages = (int)Math.Ceiling(count / (double)PageSize);
+
+            }
+            catch { }
             return Page();
         }
     }
--- Team5.Web/Pages/ManageTraineerProfile/Index.cshtml.cs
+++ Team5.Web/Pages/ManageTraineerProfile/Index.cshtml.cs
@@ -5,28 +5,68 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Team5.Application.Repository;
+using Team5.Domain.Common;
 //using Domain1;
 
 namespace Team5.Web.Pages.ManageTraineerProfile
 {
     public class IndexModel : PageModel
     {
-        //private readonly Domain1.ZooManagementContext _context;
+        private readonly IUnitOfWork _unitOfWork;
 
-        //public IndexModel(Domain1.ZooManagementContext context)
-        //{
-        //    _context = context;
-        //}
-
-        public IList<TraineerProfile> TraineerProfile { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public IndexModel(IUnitOfWork unitOfWork)
         {
-            //if (_context.TraineerProfiles != null)
-            //{
-            //    TraineerProfile = await _context.TraineerProfiles
-            //    .Include(t => t.Account).ToListAsync();
-            //}
+            _unitOfWork = unitOfWork;
+        }
+
+        public IList<TraineerProfile> TraineerProfile { get; set; } = default!;
+
+        public int PageIndex { get; set; } = 1;
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; } = 5;
+        public bool HasNextPage => PageIndex < TotalPages;
+        public bool HasPreviousPage => PageIndex > 1;
+        public string? SearchString { get; set; }
+
+        public async Task OnGetAsync(string? searchString, int? pageIndex)
+        {
+            try
+            {
+                if (pageIndex != null)
+                {
+                    PageIndex = pageIndex.Value;
+                }
+                if (searchString != null)
+                {
+                    SearchString = searchString;
+                    TraineerProfile = (await _unitOfWork.TrainerProfile.GetWithPagination(new QueryHelper<TraineerProfile>()
+                    {
+                        PagingParams = new PagingParameters(PageIndex, PageSize),
+                        OrderByFields = new List<string>
+                {
+                    $"UpdatedDate:desc"
+                }.ToArray(),
+                        Include = t => t.Include(t => t.Account)
+                    })).ToList();
+                }
+                else
+                {
+                    TraineerProfile = (await _unitOfWork.TrainerProfile.GetWithPagination(new QueryHelper<TraineerProfile>()
+                    {
+                        PagingParams = new PagingParameters(PageIndex, PageSize),
+                        OrderByFields = new List<string>
+                {
+                    $"UpdatedDate:desc"
+                }.ToArray(),
+                        Include = t => t.Include(t => t.Account)
+
+                    })).ToList();
+                }
+                var count = (await _unitOfWork.TrainerProfile.Get()).ToList().Count;
+                TotalPages = (int)Math.Ceiling(count / (double)PageSize);
+            }
+            catch { }
         }
     }
 }
