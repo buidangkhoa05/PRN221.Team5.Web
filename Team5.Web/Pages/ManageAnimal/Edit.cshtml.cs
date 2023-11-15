ca40c2e52f4f5445bb28707937b78c2d80c913ce
khoabui <buidangkhoa05@gmail.com>
2023-11-15 17:19:00 +0700

    (...) update code

--- Team5.Web/Pages/ManageAnimal/Edit.cshtml.cs
+++ Team5.Web/Pages/ManageAnimal/Edit.cshtml.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LinqKit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PRN221.Team5.Application.Service.Implement;
+using PRN221.Team5.Domain.Entity;
 using Team5.Application.Repository;
+using Team5.Domain.Common;
 //using Domain1;
 
 namespace Team5.Web.Pages.ManageAnimal
@@ -29,8 +32,10 @@ namespace Team5.Web.Pages.ManageAnimal
 
         [BindProperty]
         public Animal Animal { get; set; } = default!;
+
         [BindProperty]
         public Meal Meal { get; set; } = default!;
+
         [BindProperty]
         public Meal_Food Meal_Food { get; set; } = default!;
 
@@ -46,7 +51,14 @@ namespace Team5.Web.Pages.ManageAnimal
                 return NotFound();
             }
 
-            Animal = await _unitOfWork.Animal.GetFirstOrDefaultAsync(m => m.Id == id);
+            var animalId = id ?? Guid.Empty;
+
+            Animal = await _unitOfWork.Animal.GetById(animalId, new QueryHelper<Animal>()
+            {
+                Include = t => t.Include(t => t.Meal_Animals)
+                                    .ThenInclude(t => t.Meal)
+            });
+
             if (Animal == null)
             {
                 return NotFound();
@@ -57,15 +69,47 @@ namespace Team5.Web.Pages.ManageAnimal
             ViewData["SpecieId"] = new SelectList(await _animalSpecieService.GetAll(), "Id", "Name");
             return Page();
         }
+
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
+
             Animal.UpdatedDate = DateTime.Now;
+            Animal.UpdatedBy = User.Identity.Name;
+
             try
             {
+                var animal = await _unitOfWork.Animal.GetById(Animal.Id, new QueryHelper<Animal>()
+                {
+                    Include = t => t.Include(t => t.Meal_Animals)
+                                        .ThenInclude(t => t.Meal)
+                });
 
-                await _unitOfWork.Animal.UpdateAsync(Animal, true);
+                Animal.Meal_Animals = animal.Meal_Animals;
+
+                var mealId = await _unitOfWork.Meal.CreateAsync(Meal, true);
+                Meal_Food.MealId = mealId;
+
+                var mealFoodId = await _unitOfWork.Meal_Food.CreateAsync(Meal_Food, true);
+
+                Animal.Meal_Animals.ForEach(t => t.IsDeleted = true);
+
+                //await _unitOfWork.Animal.UpdateAsync(Animal, true);
+
+                //animal = await _unitOfWork.Animal.GetById(Animal.Id, new QueryHelper<Animal>()
+                //{
+                //    Include = t => t.Include(t => t.Meal_Animals)
+                //                        .ThenInclude(t => t.Meal)
+                //});
+
+                Animal.Meal_Animals.Add(new Meal_Animal()
+                {
+                    MealId = mealId,
+                    AnimalId = Animal.Id
+                });
+
+                await _unitOfWork.Animal.UpdateAsync(animal, true);
             }
             catch (Exception)
             {
